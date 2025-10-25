4672cc6e97a75cf44c7c7161e323d19536dd4c09
Charles793 <charlesfinlayson15@gmail.com>
2025-10-25 14:55:14 +0100

    adding database

--- guitarcomparison/guitarguitar/models.py
+++ guitarcomparison/guitarguitar/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Genres(models.Model):
+    genre = models.CharField()
+
+class Names(models.Model):
+    Names = models.CharField()
+
+class genresNames(models.Model):
+    genre = models.ForeignKey(Genres)
+    names = models.ForeignKey(Names)
+
+
