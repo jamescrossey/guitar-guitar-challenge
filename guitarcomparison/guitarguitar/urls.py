3f01d982eab4c0b19c6443c8d7203f03cc7bf221
James Crossey <2734717c@student.gla.ac.uk>
2025-10-25 18:15:29 +0100

    Spotify API working

--- guitarcomparison/guitarguitar/urls.py
+++ guitarcomparison/guitarguitar/urls.py
@@ -4,6 +4,5 @@ from guitarguitar import views
 app_name = "guitarguitar"
 url_patterns = [
     path('', views.index, name='index'),
-    path("search/", views.SearchResults, name="search_results")
 
 ]
